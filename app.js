383972ac53a26fbc3f41d742522fc9460268813d
Josef Hrebec <josefhrebec02@gmail.com>
2023-03-31 13:15:02 +0200

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
+app.use(helmet());
+
+var RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -30,6 +41,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
